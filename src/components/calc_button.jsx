8a262ab1842e61bddfe667d6f8c585e2e6397623
alex0112 <alarsen0112@gmail.com>
2017-12-08 21:08:11 -0700

    Removed extraneous constructor, will be coming back later.

--- src/components/calc_button.jsx
+++ src/components/calc_button.jsx
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import {Button} from 'react-bootstrap';
 
 export default class CalcButton extends Component {
-
-    constructor(props) {
-	super(props);
-    }
-    
     render() {
 	return(
 	    <Button>{this.props.number}</Button>
