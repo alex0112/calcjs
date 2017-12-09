f6716613e0c29cd1c4b031b1885042922400a3b0
alex0112 <alarsen0112@gmail.com>
2017-12-08 21:20:40 -0700

    Stubbed, styled, and rendered display component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Calc from './components/calc';
+import Display from './components/display';
 
 class App extends Component {
     render() {
 	return (
 	    <div className="App">
-	      <Calc />
+		<Display />
+		<Calc />
 	    </div>
 	);
     }
---
+++ src/components/display.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Display extends Component {
+    render() {
+	return(
+	    <div id="display">
+	      This is the display.
+	    </div>
+	);
+    }    
+}
