870ed80f2ee5faa164e8ba23b5269d7755168be1
alex0112 <alarsen0112@gmail.com>
2017-12-08 21:51:12 -0700

    Moved display component into calc wrapper component.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Calc from './components/calc';
-import Display from './components/display';
 
 class App extends Component {
     render() {
 	return (
 	    <div className="App">
-		<Display />
 		<Calc />
 	    </div>
 	);
--- src/components/calc.jsx
+++ src/components/calc.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import ButtonGrid from './button_grid';
+
+import Display from './display';
+import ButtonGrid from'./button_grid';
 
 export default class Calc extends Component {
     render() {
 	return(
 	    <div id="calc-body">
+	      <Display />
 	      <ButtonGrid/>
 	    </div>
 	);
