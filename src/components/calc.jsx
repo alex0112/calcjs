dc0ac6ba2397d93be0b33aa3366e1e1011e5456a
alex0112 <alarsen0112@gmail.com>
2017-12-08 19:11:04 -0700

    Fleshed out a basic component

---
+++ src/components/calc.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Calc extends Component {
+    render() {
+	return(
+	    <div id="calc-body">
+	      
+	    </div>
+	);
+    }
+}
