69f0d7a6a6df19e7cd22cacd4d9b9c6e88d0d8d3
alex0112 <alarsen0112@gmail.com>
2017-12-13 17:59:35 -0700

    Buttons now update screen component with their values.

--- src/components/button_grid.jsx
+++ src/components/button_grid.jsx
@@ -3,25 +3,24 @@ import CalcButton from './calc_button';
 import {Grid, Row, Col} from 'react-bootstrap';
 
 export default class ButtonGrid extends Component {
-    
     render() {
 	console.log(this.props);
 	return(
 	    <Grid>
 	      <Row className="show-grid">
 		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen} number={7}/></Col>
-
-		<Col xs={4}> <CalcButton number={9}/></Col>
+		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen} number={8}/></Col>
+		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={9}/></Col>
 	      </Row>
 	      <Row className="show-grid">
-		<Col xs={4}> <CalcButton number={4} /></Col>
-		<Col xs={4}> <CalcButton number={5}/></Col>
-		<Col xs={4}> <CalcButton number={6}/></Col>
+		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={4} /></Col>
+		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={5}/></Col>
+		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={6}/></Col>
 	      </Row>
 	      <Row className="show-grid">
-		<Col xs={4} md={4}> <CalcButton number={1}/></Col>
-		<Col xs={4} md={4}> <CalcButton number={2}/></Col>
-		<Col xs={4} md={4}> <CalcButton number={3}/></Col>
+		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={1}/></Col>
+		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={2}/></Col>
+		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={3}/></Col>
 	      </Row>
 	    </Grid>
 	);
