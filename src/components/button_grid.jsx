4e2a4d69a5c1c76812587851d4650b05d644a005
alex0112 <alarsen0112@gmail.com>
2017-12-15 11:44:32 -0700

    Added more buttons

--- src/components/button_grid.jsx
+++ src/components/button_grid.jsx
@@ -7,19 +7,32 @@ export default class ButtonGrid extends Component {
 	return(
 	    <Grid>
 	      <Row className="show-grid">
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen} number={7}/></Col>
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen} number={8}/></Col>
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={9}/></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen} number={'C'} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen} number={'u'} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'u'} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'+'} /></Col>
 	      </Row>
 	      <Row className="show-grid">
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={4} /></Col>
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={5}/></Col>
-		<Col xs={4}> <CalcButton updateScreen={this.props.updateScreen}  number={6}/></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen} number={7} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen} number={8} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={9} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'-'} /></Col>
 	      </Row>
 	      <Row className="show-grid">
-		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={1}/></Col>
-		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={2}/></Col>
-		<Col xs={4} md={4}> <CalcButton updateScreen={this.props.updateScreen}  number={3}/></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={4} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={5} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={6} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'/'} /></Col>
+	      </Row>
+	      <Row className="show-grid">
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={1} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={2} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={3} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'*'} /></Col>
+	      </Row>
+	      <Row className="show-grid">
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={0} /></Col>
+		<Col xs={3}> <CalcButton updateScreen={this.props.updateScreen}  number={'='} /></Col>
 	      </Row>
 	    </Grid>
 	);
