8ebb606db06c9f3a021444e42bc4b5db0093783b
alex0112 <alarsen0112@gmail.com>
2017-12-15 11:46:56 -0700

    Added a lot of logic in updateScreen that will eventually be abstracted away into something else.

--- src/components/calc.jsx
+++ src/components/calc.jsx
@@ -8,32 +8,70 @@ import Expr from '../utils/expr';
 export default class Calc extends Component {
     constructor(props) {
 	super(props);
-
-	this.state = {
+	
+	 this.state = {
 	    current_num: 0,
 	    expr: new Expr()
 	};
 	this.updateScreen = this.updateScreen.bind(this);
     }
-
+    
     updateScreen(val) {
+	
 	let expr = this.state.expr;
-	if (val ===~ /\+-\/\\*/) { // i.e. is it an operand?
-	    
+	console.log("updateScreen(): ", val);
+	console.log("Before:", this.state.current_num);
+	if (val === 'C') {
+	    this.setState({
+		current_num: 0,
+		expr: new Expr()
+	    });
 	}
+
+	else if (val === '=') {
+	    if (expr.a && expr.b) {
+		const result = expr.eval();
+		this.setState({
+		    current_num: result,
+		});
+	    }
+	}
+	
+	else if (val === 'u') {
+	    // do nothing.
+	}
+	
+	else if (val ===~ /\+-\/\\*/) { // i.e. is it an operand?
+	    console.log("Operand:", val);
+	    expr.operand = val;
+	    this.setState({
+		expr: expr
+	    });
+
+	}
+	
 	else {
-	    if (this.state.expr.operand == null) {
+	    if (this.state.expr.operand === null) {
 		expr.a = val;
-		this.setState({ current_num: expr.a });
+		this.setState({
+		    expr: expr,
+		    current_num: expr.a
+		});
 	    }
 	    else {
+		console.log('setting b');
 		expr.b = val;
-		this.setState({ current_num: expr.b });
+		this.setState({
+		    expr: expr,
+		    current_num: expr.b
+		});
 	    }
 	}
+	console.log("After:", this.state.current_num);
     }
     
     render() {
+	console.log("foo", this.state.current_num);
 	return(
 	    <div id="calc-body">
 	      <Display currentNum={this.state.current_num} />
