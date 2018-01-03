b8a368fd09195f01bdc464b2c34799042bade80f
alex0112 <alarsen0112@gmail.com>
2018-01-02 20:23:37 -0700

    Added some TODO comments.

--- src/components/calc_button.jsx
+++ src/components/calc_button.jsx
@@ -5,7 +5,7 @@ export default class CalcButton extends Component {
     render() {
 	return(
 	    <Button
-	      bsStyle={this.props.type}
+	      bsStyle={this.props.type} // TODO: Find the proper way to style bootstrap buttons with custom classes. (https://github.com/alex0112/calcjs/issues/4)
 	      onClick={() => this.props.updateScreen(this.props.number)}>
 	      <span className="number">{this.props.number}</span>
 	    </Button>
